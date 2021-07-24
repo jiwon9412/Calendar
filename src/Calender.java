a13c3e4e5b1ca59befa79ea1e05b33e641ca3cdf
지원 <@VAIO>
2021-07-24 23:33:01 +0900

    조건문 활용하여 최대일수구하기 추가

--- src/Calender.java
+++ src/Calender.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Calender {
 
 	public static void main(String[] args) {
@@ -9,7 +11,21 @@ public class Calender {
 		System.out.println(" 8  9 10 11 12 13 14");
 		System.out.println("15 16 17 18 19 20 21");
 		System.out.println("22 23 24 25 26 27 28");
-
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("달을 입력하세요.");
+		int m = sc.nextInt();
+		int max = 0;
+		if(m==1||m==3||m==5||m==7||m==8||m==10||m==12) {
+			max = 31;
+		}
+		else if (m == 2) {
+			max = 28;
+		}
+		else {
+			max = 30;
+		}
+		System.out.printf("%d월은 %d일까지 있습니다.\n", m,max);
 	}
 
 }
